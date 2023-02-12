e56ec498dc08038d3e3f9ecac13df93023d2bcf4
zenxero <87404368+zenxero@users.noreply.github.com>
2023-02-12 15:12:33 -0800

    Simplifying winget install script

--- winget-installs.ps1
+++ winget-installs.ps1
@@ -1,38 +1,38 @@
-#######################################
-# Install apps with winget
-#######################################
+#
+# Winget Package Installs
+#
 
 @"
 
-Installing apps with winget
+Installing Winget Packages
 ----------------------------------------
 "@
 
 Write-Output "Installing Apps"
 $wingetapps = @(
-    @{name = "7zip.7zip" },
-    @{name = "CPUID.CPU-Z" },
-    @{name = "CrystalDewWorld.CrystalDiskInfo" },
-    @{name = "Discord.Discord" },
-    @{name = "Git.Git" },
-    @{name = "Google.Chrome" },
-    @{name = "Microsoft.PowerToys" },
-    @{name = "Microsoft.VisualStudioCode" },
-    @{name = "Microsoft.WindowsTerminal" },
-    @{name = "Mozilla.Firefox" },
-    @{name = "RaspberryPiFoundation.RaspberryPiImager" },
-    @{name = "REALiX.HWiNFO" },
-    @{name = "TechPowerUp.GPU-Z" },
-    @{name = "VideoLAN.VLC" },
-    @{name = "Microsoft.VCRedist.2008.x86" },
-    @{name = "Microsoft.VCRedist.2008.x64" },
-    @{name = "Microsoft.VCRedist.2010.x86" },
-    @{name = "Microsoft.VCRedist.2010.x64" },
-    @{name = "Microsoft.VCRedist.2012.x86" },
-    @{name = "Microsoft.VCRedist.2012.x64" },
-    @{name = "Microsoft.VCRedist.2015+.x86" },
-    @{name = "Microsoft.VCRedist.2015+.x64" }
-);
+    "7zip.7zip"
+    "CPUID.CPU-Z"
+    "CrystalDewWorld.CrystalDiskInfo"
+    "Discord.Discord"
+    "Git.Git"
+    "Google.Chrome"
+    "Microsoft.PowerToys"
+    "Microsoft.VisualStudioCode"
+    "Microsoft.WindowsTerminal"
+    "Mozilla.Firefox"
+    "RaspberryPiFoundation.RaspberryPiImager"
+    "REALiX.HWiNFO"
+    "TechPowerUp.GPU-Z"
+    "VideoLAN.VLC"
+    "Microsoft.VCRedist.2008.x86"
+    "Microsoft.VCRedist.2008.x64"
+    "Microsoft.VCRedist.2010.x86"
+    "Microsoft.VCRedist.2010.x64"
+    "Microsoft.VCRedist.2012.x86"
+    "Microsoft.VCRedist.2012.x64"
+    "Microsoft.VCRedist.2015+.x86"
+    "Microsoft.VCRedist.2015+.x64"
+)
 
 # We need to interact with winget and accept the source agreements
 # before we're able to actually use it. So, just a random search
@@ -40,12 +40,5 @@ $wingetapps = @(
 winget search Microsoft.WindowsTerminal --accept-source-agreements
 
 foreach ($wingetapp in $wingetapps) {
-    $listApp = winget list --exact -q $wingetapp.name
-    if (![String]::Join("", $listApp).Contains($wingetapp.name)) {
-        Write-host "Installing: " $wingetapp.name
-        winget install -e --accept-source-agreements --accept-package-agreements --id $wingetapp.name 
-    }
-    else {
-        Write-host "Skipping: " $wingetapp.name " (already installed)"
-    }
+    winget install -e --accept-source-agreements --accept-package-agreements --id $wingetapp
 }
