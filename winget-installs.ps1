f936b540b8a3d7265510fa0180292e11b6d866b5
zenxero <87404368+zenxero@users.noreply.github.com>
2023-02-28 17:03:34 -0800

    Removing all 3rd party apps from winget

--- winget-installs.ps1
+++ winget-installs.ps1
@@ -10,20 +10,7 @@ Installing Winget Packages
 
 Write-Output "Installing Apps"
 $wingetapps = @(
-    "7zip.7zip"
-    "CPUID.CPU-Z"
-    "CrystalDewWorld.CrystalDiskInfo"
-    "Discord.Discord"
-    "Git.Git"
-    "Google.Chrome"
-    "Microsoft.PowerToys"
-    "Microsoft.VisualStudioCode"
     "Microsoft.WindowsTerminal"
-    "Mozilla.Firefox"
-    "RaspberryPiFoundation.RaspberryPiImager"
-    "REALiX.HWiNFO"
-    "TechPowerUp.GPU-Z"
-    "VideoLAN.VLC"
     "Microsoft.VCRedist.2008.x86"
     "Microsoft.VCRedist.2008.x64"
     "Microsoft.VCRedist.2010.x86"
@@ -33,6 +20,32 @@ $wingetapps = @(
     "Microsoft.VCRedist.2015+.x86"
     "Microsoft.VCRedist.2015+.x64"
 )
+
+# Write-Output "Installing Apps"
+# $wingetapps = @(
+#     "7zip.7zip"
+#     "CPUID.CPU-Z"
+#     "CrystalDewWorld.CrystalDiskInfo"
+#     "Discord.Discord"
+#     "Git.Git"
+#     "Google.Chrome"
+#     "Microsoft.PowerToys"
+#     "Microsoft.VisualStudioCode"
+#     "Microsoft.WindowsTerminal"
+#     "Mozilla.Firefox"
+#     "RaspberryPiFoundation.RaspberryPiImager"
+#     "REALiX.HWiNFO"
+#     "TechPowerUp.GPU-Z"
+#     "VideoLAN.VLC"
+#     "Microsoft.VCRedist.2008.x86"
+#     "Microsoft.VCRedist.2008.x64"
+#     "Microsoft.VCRedist.2010.x86"
+#     "Microsoft.VCRedist.2010.x64"
+#     "Microsoft.VCRedist.2012.x86"
+#     "Microsoft.VCRedist.2012.x64"
+#     "Microsoft.VCRedist.2015+.x86"
+#     "Microsoft.VCRedist.2015+.x64"
+# )
 
 # We need to interact with winget and accept the source agreements
 # before we're able to actually use it. So, just a random search
