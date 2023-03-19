82b3aadfae82a08d486e22ffb730c20abd508d75
zenxero <87404368+zenxero@users.noreply.github.com>
2023-03-19 01:10:19 -0700

    Adding Requires -RunAsAdministrator and alternate way to accept the winget agreements

--- winget-installs.ps1
+++ winget-installs.ps1
@@ -2,6 +2,8 @@
 # Winget Package Installs
 #
 
+#Requires -RunAsAdministrator
+
 @"
 
 Installing Winget Packages
@@ -21,36 +23,11 @@ $wingetapps = @(
     "Microsoft.VCRedist.2015+.x64"
 )
 
-# Write-Output "Installing Apps"
-# $wingetapps = @(
-#     "7zip.7zip"
-#     "CPUID.CPU-Z"
-#     "CrystalDewWorld.CrystalDiskInfo"
-#     "Discord.Discord"
-#     "Git.Git"
-#     "Google.Chrome"
-#     "Microsoft.PowerToys"
-#     "Microsoft.VisualStudioCode"
-#     "Microsoft.WindowsTerminal"
-#     "Mozilla.Firefox"
-#     "RaspberryPiFoundation.RaspberryPiImager"
-#     "REALiX.HWiNFO"
-#     "TechPowerUp.GPU-Z"
-#     "VideoLAN.VLC"
-#     "Microsoft.VCRedist.2008.x86"
-#     "Microsoft.VCRedist.2008.x64"
-#     "Microsoft.VCRedist.2010.x86"
-#     "Microsoft.VCRedist.2010.x64"
-#     "Microsoft.VCRedist.2012.x86"
-#     "Microsoft.VCRedist.2012.x64"
-#     "Microsoft.VCRedist.2015+.x86"
-#     "Microsoft.VCRedist.2015+.x64"
-# )
-
 # We need to interact with winget and accept the source agreements
 # before we're able to actually use it. So, just a random search
 # command will work.
-winget search Microsoft.WindowsTerminal --accept-source-agreements
+# winget search Microsoft.WindowsTerminal --accept-source-agreements
+echo Y | winget list | Out-Null
 
 foreach ($wingetapp in $wingetapps) {
     winget install -e --accept-source-agreements --accept-package-agreements --id $wingetapp
